a07909852998be5050240412e18fb373efe5a906
ViktorMakTorwend <vikmactor@gmail.com>
2024-10-15 19:12:01 +0300

    Some fixes

--- Front-end/src/app/components/pony-type-ahead/pony-type-ahead.component.ts
+++ Front-end/src/app/components/pony-type-ahead/pony-type-ahead.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { PonyModel } from '../../types/ponyModel';
 import { HTTP_PoniesService } from '../../services/HTTP-ponies.service';
-import { concatMap, debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+import { catchError, concatMap, debounceTime, distinctUntilChanged, filter, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'eva-pony-type-ahead',
@@ -29,19 +29,19 @@ export class PonyTypeAheadComponent implements OnInit {
     // with the concatMap operator all the values emited
     // this.input.valueChanges
     //   .pipe(concatMap(value => this.ponyService.searchPony(value)))
-    //   .subscribe(response => this.ponies = response)
+    //   .subscribe(response => this.ponies = response);
 
     // with the switchMap operator only last value emited
     // this.input.valueChanges
     //  .pipe(switchMap(value => this.ponyService.searchPony(value)))
-    //  .subscribe(response => this.ponies = response)
+    //  .subscribe(response => this.ponies = response);
 
     // discard the queries that are less than three characters
     // this.input.valueChanges
     //   .pipe(
     //     filter(query => query.length >= 3),
     //     switchMap(value => this.ponyService.searchPony(value)))
-    //   .subscribe(response => this.ponies = response)
+    //   .subscribe(response => this.ponies = response);
 
     // with delay
     // this.input.valueChanges
@@ -49,15 +49,26 @@ export class PonyTypeAheadComponent implements OnInit {
     //     filter(query => query.length >= 3),
     //     debounceTime(400),
     //     switchMap(value => this.ponyService.searchPony(value)))
-    //   .subscribe(response => this.ponies = response)
+    //   .subscribe(response => this.ponies = response);
 
     // to exclude subsequent requests
+    // this.input.valueChanges
+    //   .pipe(
+    //     filter(query => query.length >= 3),
+    //     debounceTime(400),
+    //     distinctUntilChanged(),
+    //     switchMap(value => this.ponyService.searchPony(value)))
+    //   .subscribe(response => this.ponies = response);
+
+    // with error handle
     this.input.valueChanges
-      .pipe(
-        filter(query => query.length >= 3),
-        debounceTime(400),
-        distinctUntilChanged(),
-        switchMap(value => this.ponyService.searchPony(value)))
-      .subscribe(response => this.ponies = response)
+    .pipe(
+      filter(query => query.length >= 3),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(value => this.ponyService
+        .searchPony(value)
+        .pipe(catchError(error => of([])))))
+    .subscribe(response => this.ponies = response);
   }
 }
